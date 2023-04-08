b281cb83dfab8534f26ec89cb4884002de37fcd2
Aditi <aditikumari9663@gmail.com>
2023-04-08 12:08:49 +0530

    Adding files of the project

---
+++ app/src/main/java/com/aditi/ourfirstapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aditi.ourfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
